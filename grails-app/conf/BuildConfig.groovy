d15bba700134dbae06b69d0b17b5d5906aab86d0
Dave Klein <daveklein@usa.net>
2014-09-16 14:58:13 -0700

    HL-2224: Fixing dependency error in BuildConfig.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -31,7 +31,7 @@ grails.project.dependency.resolution = {
   }
   dependencies {
     // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-    compile 'org.apache.commons:commons-io:2.4'
+    compile 'commons:commons-io:2.4'
 
     // runtime 'mysql:mysql-connector-java:5.1.18'
   }
